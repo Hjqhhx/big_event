c5906483610f66d4663300942b2980d380088e66
xhjqhhh <2143013546@qq.com>
2024-07-07 19:18:31 +0800

    添加knife4j用于模仿前端请求的API

--- src/main/java/com/itheima/controller/UserController.java
+++ src/main/java/com/itheima/controller/UserController.java
@@ -4,6 +4,8 @@ import cn.hutool.core.util.ObjectUtil;
 import com.itheima.pojo.Result;
 import com.itheima.pojo.User;
 import com.itheima.service.UserService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -11,25 +13,32 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/user")
+@Tag(name = "用户API")
 public class UserController {
 
     @Autowired
     private UserService userService;
 
+    @Operation(summary = "注册请求")
     @PostMapping("/register")
     public Result register(String username, String password) {
 
-        //查询用户
-        User user = userService.findByUserName(username);
-        //判断要注册的对象是否已经存在
-        if (ObjectUtil.isEmpty(user)) {
-            //没有占用，进行注册操作
-            userService.register(username, password);
-            return Result.success();
-        } else {
-            //占用，返回提示信息
-            return Result.error("该用户名已存在");
+        //判断username，和password是否满足条件
+        if (username != null && username.length() > 5 && username.length() <= 16 && password != null && password.length() >= 5 && username.length() <= 16) {
+            //查询用户
+            User user = userService.findByUserName(username);
+            //判断要注册的对象是否已经存在
+            if (ObjectUtil.isEmpty(user)) {
+                //没有占用，进行注册操作
+                userService.register(username, password);
+                return Result.success();
+            } else {
+                //占用，返回提示信息
+                return Result.error("该用户名已存在");
+            }
         }
+
+        return Result.error("参数不符合要求");
 
     }
 }
